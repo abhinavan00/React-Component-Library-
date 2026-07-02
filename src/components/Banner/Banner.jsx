0a5c4fe86a14e90f0793c442e68ec21bc9686d2f
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-02 13:25:29 +0530

    Basic Skeleton of Banner complete

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,12 +3,15 @@ import './Banner.css'
 
 export default function Banner() {
     return (
-        <div className='banner-container'>
-            <img src={successIcon} alt='success icon' />
+        <div className='banner'>
             <div>
-                <p>Congratulations!</p>
-                <p></p>
+                <img src={successIcon} alt='success icon' />
+                <p className='msg'>Congratulations!</p>
             </div>
+            <p className='msg-detail'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Aliquid pariatur, ipsum similique veniam.
+            </p>
         </div>
     )
 }
