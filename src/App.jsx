860dfa482690c176c1bb68e6e94a1295e53ce692
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-03 11:36:25 +0530

    Card Component Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Badge from './components/Badge/Badge.jsx'
 import Banner from './components/Banner/Banner.jsx'
+import Card from './components/Card/Card.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Card />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,1 +1,20 @@
+import cloudUploadIcon from '../../assets/Card-Icons/cloud-upload-icon.svg'
 import './Card.css'
+
+export default function Card() {
+
+    return (
+        <div className='card-container'>
+            <div className='card-content'>
+                <p className='title'>Easy Deployment</p>
+                <p className='text'>
+                    Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. 
+                    Et magna sit morbi lobortis.
+                </p>
+                <div className='upload-icon'>
+                    <img src={cloudUploadIcon} alt='cloud upload icon' />
+                </div>
+            </div>
+        </div>
+    )
+}
