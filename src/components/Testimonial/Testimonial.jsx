d909d7c2c89773232c0e0ba89977cd2f9f925287
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-04 15:22:08 +0530

    testimonial component type 2 done

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -3,24 +3,23 @@ import './Testimonial.css'
 
 export default function Testimonial({src, variant}) {
     if(src) {
-        if(variant === 'desktop') {
-            return (
-                <div className='testimonial-with-img'>
-                    <div className='testimonial-details'>
-                        <img src={quoteIcon} alt='quote icon'/>
-                        <p className='text'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Sed urna nulla vitae laoreet augue. Amet feugiat est integer 
-                            dolor auctor adipiscing nunc urna, sit. 
-                        </p>
-                        <div>
-                            <p className='person-name'>May Andersons</p>
-                            <p className='person-role'>Workcation, CTO</p>
-                        </div>
+        const desktopClass = variant === 'desktop' ? 'desktop' : ''
+        return (
+            <div className={`testimonial-with-img ${desktopClass}`}>
+                <div className={`testimonial-details ${desktopClass}`}>
+                    <img src={quoteIcon} alt='quote icon'/>
+                    <p className='text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Sed urna nulla vitae laoreet augue. Amet feugiat est integer 
+                        dolor auctor adipiscing nunc urna, sit. 
+                    </p>
+                    <div>
+                        <p className='person-name'>May Andersons</p>
+                        <p className='person-role'>Workcation, CTO</p>
                     </div>
-                    <img src={src} alt='testimonial person image' className='person-img'/>
                 </div>
-            )
-        }
+                <img src={src} alt='testimonial person image' className={`person-img ${desktopClass}`}/>
+            </div>
+        )   
     }   
 }
