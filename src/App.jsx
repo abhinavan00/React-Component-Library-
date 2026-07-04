43e6d3bf2953d33d5148e81c610ad5c40b22f795
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-05 05:18:28 +0530

    Understood basic of creating tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Banner from './components/Banner/Banner.jsx'
 import Card from './components/Card/Card.jsx'
 import Testimonial from './components/Testimonial/Testimonial.jsx'
 import testimonialImg from './assets/Testimonial-Assets/testimonial-img.jpg'
+import Tooltip from './components/Tooltip/Tooltip.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Tooltip />
     </>
   )
 }
--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,5 +1,7 @@
 import './Tooltip.css'
 
 export default function Tooltip() {
-    
+    return (
+        <div className='arrow-down'></div>
+    )
 }
