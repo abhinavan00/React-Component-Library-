fa2b85c1c2e98ad033327d313f94033ce4606d17
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-04 14:54:34 +0530

    Testimonial Component 01 complete

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -5,20 +5,20 @@ export default function Testimonial({src, variant}) {
     if(src) {
         if(variant === 'desktop') {
             return (
-                <div className='desktop-testimonial'>
-                    <div className='desktop-testimonial-details'>
+                <div className='testimonial-with-img'>
+                    <div className='testimonial-details'>
                         <img src={quoteIcon} alt='quote icon'/>
-                        <p>
+                        <p className='text'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Sed urna nulla vitae laoreet augue. Amet feugiat est integer 
                             dolor auctor adipiscing nunc urna, sit. 
                         </p>
                         <div>
-                            <p>May Andersons</p>
-                            <p>Workcation, CTO</p>
+                            <p className='person-name'>May Andersons</p>
+                            <p className='person-role'>Workcation, CTO</p>
                         </div>
                     </div>
-                    <img src={src} alt='testimonial person image'/>
+                    <img src={src} alt='testimonial person image' className='person-img'/>
                 </div>
             )
         }
