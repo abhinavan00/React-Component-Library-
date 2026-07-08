0b42a6edaaacfa81cd22a5421bccd8f91e3ab9a9
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-08 07:23:53 +0530

    Base design of tooltip complete

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,7 +1,21 @@
+import { LuInbox } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
 import './Tooltip.css'
 
 export default function Tooltip() {
     return (
-        <div className='arrow-down'></div>
+       <div className="tooltip">
+            <RxCross2 className="tooltip-close-btn"/>
+            <div className="tooltip-details">
+                <div className="tooltip-title">
+                    <LuInbox className="tooltip-icon"/>
+                    <span>Archive notes</span>
+                </div>
+                <p className="tooltip-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.
+                </p>
+            </div>
+            <div className="tip"></div>
+       </div>
     )
 }
