fdfdda035f3e29bc9f888d3345b428d6c404e1e4
Abhinav Anand <abhinavanand072@gmail.com>
2026-07-08 08:12:57 +0530

    created 2 dark versions

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <Tooltip />
+      <Tooltip variant={''}/>
     </>
   )
 }
--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -2,20 +2,20 @@ import { LuInbox } from "react-icons/lu";
 import { RxCross2 } from "react-icons/rx";
 import './Tooltip.css'
 
-export default function Tooltip() {
+export default function Tooltip({variant}) {
     return (
-       <div className="tooltip">
-            <RxCross2 className="tooltip-close-btn"/>
-            <div className="tooltip-details">
+       <div className={`tooltip ${variant}`}>
+            <RxCross2 className={`tooltip-close-btn`}/>
+            <div>
                 <div className="tooltip-title">
-                    <LuInbox className="tooltip-icon"/>
-                    <span>Archive notes</span>
+                    <LuInbox className={`tooltip-icon`}/>
+                    <span className={variant}>Archive notes</span>
                 </div>
-                <p className="tooltip-text">
+                <p className={`tooltip-text`}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.
                 </p>
             </div>
-            <div className="tip"></div>
+            <div className={`tip`}></div>
        </div>
     )
 }
